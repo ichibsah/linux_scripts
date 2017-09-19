23a16a2ef0e479cd0e11e24567ee2ba9e7ec9e48
ichibsah <info@skylinewebservice.de>
2017-09-19 16:20:29 +0200

    19.09.2017 16:20:29,59

---
+++ github-fork-update.bat
@@ -0,0 +1,12 @@
+cd E:\Sandbox\linux_scripts
+git add .
+git commit -m "%date% %time%"
+git remote add origin https://github.com/ichibsah/linux_scripts.git
+git push -u origin master
+
+
+git remote add upstream https://github.com/tiiiecherle/linux_scripts.git
+git fetch upstream
+git pull upstream master
+
+pause
