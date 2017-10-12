034660dee7bae8e76afcd702b977e4a39b38c9da
ichibsah <info@skylinewebservice.de>
2017-10-12 11:24:22 +0200

    12.10.2017 11:24:22,76

--- github-fork-update.bat
+++ github-fork-update.bat
@@ -2,6 +2,7 @@ REM cd E:\Sandbox\linux_scripts
 git add .
 git commit -m "%date% %time%"
 git remote add origin https://github.com/ichibsah/linux_scripts.git
+git pull https://github.com/ichibsah/linux_scripts.git
 git push -u origin master
 
 
